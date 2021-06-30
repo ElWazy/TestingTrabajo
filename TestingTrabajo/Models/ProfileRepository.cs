4831d6e31086607832e2f7e35cd138bec121df83
ElWazy <santiago.fierro4@outlook.cl>
2021-06-29 23:43:40 -0400

    Obtenemos todos los perfiles de db

---
+++ TestingTrabajo/Models/ProfileRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestingTrabajo.Models
+{
+    interface ProfileRepository
+    {
+        List<Profile> GetAll();
+    }
+}
---
+++ TestingTrabajo/Persistence/MariaDBProfileRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using MySqlConnector;
+using System.Collections.Generic;
+using TestingTrabajo.Models;
+
+namespace TestingTrabajo.Persistence
+{
+    class MariaDBProfileRepository : ProfileRepository
+    {
+        private MySqlConnection connection = null;
+
+        public MariaDBProfileRepository(string url)
+        {
+            connection = new MySqlConnection(url);
+        }
+
+        public List<Profile> GetAll()
+        {
+            string sql = @"SELECT uuid, name FROM profile 
+                        ORDER BY name ASC";
+
+            List<Profile> employees = null;
+            connection.Open();
+
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            command.Prepare();
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            if (!reader.HasRows)
+            {
+                return null;
+            }
+
+            employees = new List<Profile>();
+            while (reader.Read())
+            {
+                employees.Add(
+                    new Profile(
+                        reader.GetString(1), reader.GetString(2)
+                        )
+                    );
+            }
+
+            return employees;
+        }
+    }
+}
