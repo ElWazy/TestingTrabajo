7261fab25a9bd196283e396fd4e58834ec0d9d61
Salomovich <esteban.vivanco.progra@gmail.com>
2021-06-29 23:24:00 -0400

    Class Tool, Interface ToolRepositoy y Persistence MariaDBToolRepositoy

---
+++ TestingTrabajo/Models/ToolRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingTrabajo.Models
+{
+    interface ToolRepository
+    {
+
+        string getName();
+
+        Tool AddTool(string uuid, string name, int category_id_fk, int stock, int real_stock);
+        Tool UpdateTool(string uuid, string name, int category_id_fk, int stock, int real_stock);
+
+    }
+
+}
